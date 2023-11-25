a4097252265264ec5627c350aa3e828253cb41b6
sivin <120440687+Sivin-Wi@users.noreply.github.com>
2023-11-25 20:44:29 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -6,27 +6,22 @@ const btn5 = document.getElementById("div-5");
 const formSubmit = document.getElementById("formSubmit");
 
 btn1.addEventListener("click", () => {
-    btn1Value = btn1.getAttribute("value");
     btn1.style.backgroundColor = "hsl(25, 97%, 53%)";
 })
 
 btn2.addEventListener("click", () => {
-    btn2Value = btn2.getAttribute("value");
     btn2.style.backgroundColor = "hsl(25, 97%, 53%)";
 })
 
 btn3.addEventListener("click", () => {
-    btn3Value = btn3.getAttribute("value");
     btn3.style.backgroundColor = "hsl(25, 97%, 53%)";
 })
 
 btn4.addEventListener("click", () => {
-    btn4Value = btn4.getAttribute("value");
     btn4.style.backgroundColor = "hsl(25, 97%, 53%)";
 })
 
 btn5.addEventListener("click", () => {
-    btn5Value = btn5.getAttribute("value");
     btn5.style.backgroundColor = "hsl(25, 97%, 53%)";
 })
 
